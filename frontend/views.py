aaf1b8cf79a8447c7a72ef0357607c22c0fe3420
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2021-09-17 12:37:42 +0000

    rendering html w/python var

--- frontend/views.py
+++ frontend/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-    return HttpResponse('hello world')
+    return render(request, 'hello.html', { 'name': 'ash'})
