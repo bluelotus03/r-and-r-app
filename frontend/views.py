a7f4a5e0a6b3cec178f0152d45c2562bba16a1de
bluelotus03 <25125692+bluelotus03@users.noreply.github.com>
2021-09-17 14:43:00 +0000

    diplaying all db rows in console & web

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Users
 
 # Create your views here.
 
 def homepage(request):
-    return render(request, 'hello.html', { 'name': 'ash'})
+    users_query_set = Users.objects.all()
+    print('#'*25)
+    print('# All DB row items')
+    for user in users_query_set:
+        print('{} {}: {}'.format(user.fname, user.lname, user.email))
+    print('#'*25)
+    return render(request, 'hello.html', { 'name': 'ash', 'users': users_query_set})
