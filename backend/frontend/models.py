e1549c472a16d45054dca29e70212c5734966fdb
lil-p3tri3 <aharmo23@uncc.edu>
2021-10-16 18:03:04 -0400

    resetting for video

--- backend/frontend/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Users(models.Model):
-    fname = models.CharField(max_length=60)
-    lname = models.CharField(max_length=60)
-    email = models.CharField(max_length=100, unique=True)
