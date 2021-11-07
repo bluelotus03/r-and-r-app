81e0f83c655f06021ef6c10939a8e254d3e2ca32
lil-p3tri3 <aharmo23@uncc.edu>
2021-11-07 17:50:06 -0500

    removed green class, try again later

--- webapp_vue/src/App.vue
+++ webapp_vue/src/App.vue
@@ -46,7 +46,7 @@
           <div class="navbar-item">
             <div class="buttons">
               <router-link to="/log-in" class="button blue">Log in</router-link>
-              <router-link to="/cart" class="button green is-success">
+              <router-link to="/cart" class="button is-success">
                 <span class="icon"><i class="fas fa-shopping-cart"></i></span>
                 <span>Cart ({{ cartTotalLength }})</span>
               </router-link>
