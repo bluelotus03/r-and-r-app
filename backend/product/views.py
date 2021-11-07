be2138fdb43ec1c898ea3071d1e1696ec9155378
lil-p3tri3 <aharmo23@uncc.edu>
2021-11-07 14:42:25 -0500

    added search bar

--- backend/product/views.py
+++ backend/product/views.py
@@ -1,7 +1,12 @@
+# Using Q (query) function from Django 
+from django.db.models import Q
+
 from django.http import Http404
+from rest_framework.serializers import Serializer
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from .models import Category, Product
 from .serializers import ProductSerializer, CategorySerializer
@@ -36,3 +41,15 @@ class CategoryDetail(APIView):
         category = self.get_object(category_slug)
         serializer = CategorySerializer(category)
         return Response(serializer.data)
+
+# Search functionality
+@api_view(['POST'])
+def search(request):
+    query = request.data.get('query', '')
+
+    if query: 
+        products = Product.objects.filter(Q(name_icontaines=query) | Q(description_icontains=query))
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data)
+    else: 
+        return Response({"products: []"})
--- webapp_vue/src/App.vue
+++ webapp_vue/src/App.vue
@@ -18,6 +18,25 @@
       </div>
 
       <div class="navbar-menu" id="navbar-menu" v-bind:class="{'is-active': showMobileMenu}">
+        <div class="navbar-start">
+          <div class="navbar-item">
+            <form method="get" action="/search">
+              <div class="field has-addons">
+                <div class="control">
+                  <input type="text" class="input" placeholder="Search for an item" name="query">
+                </div>
+                <div class="control">
+                  <button class="button is-successful">
+                    <span class="icon">
+                      <i class="fas fa-search"></i>
+                    </span>
+                  </button>
+                </div>  
+              </div>
+            </form>
+          </div>
+        </div>
+        
         <div class="navbar-end">
           <router-link to="/furniture" class="navbar-item">Furniture</router-link>
           <router-link to="/clothing" class="navbar-item">Clothing</router-link>
